21a6c07c266777b7134edbdff88354e77379fc58
Jibran <you@example.com>
2023-06-07 00:05:20 +0500

    updated

--- modules/test/main.tf
+++ modules/test/main.tf
@@ -1,8 +1,12 @@
-resource null_resource "pwd" {
-  triggers = {
-    always_run = "${uuid()}"
-  }
-  provisioner "local-exec" {
-    command = "echo $(ls)"
-  }
+# resource null_resource "pwd" {
+#   triggers = {
+#     always_run = "${uuid()}"
+#   }
+#   provisioner "local-exec" {
+#     command = "echo $(ls)"
+#   }
+# }
+
+module "child" {
+  source = "git::https://github.com/Jibran19983/tf_modules.git//modules/child?ref=master"
 }
